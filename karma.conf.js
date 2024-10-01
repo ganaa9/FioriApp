ee8f7928e7255d65a41d87d33afd7585311ee78a
Eya Chouchène <eya.chouchene.01@gmail.com>
2024-10-01 14:33:02 +0000

    a

--- karma.conf.js
+++ karma.conf.js
@@ -1,23 +1,10 @@
 module.exports = function(config) {
   config.set({
-    frameworks: ['jasmine', 'requirejs'],
-   files: [
-    // Your other file patterns...
-    'test/unit/**/*.js' // Adjust if necessary
-],
-
-    reporters: ['progress', 'coverage', 'junit'],
-    browsers: ['chrome'],
-
-    singleRun: true,
-    coverageReporter: {
-      type: 'html',
-      dir: 'coverage/'
-    },
-    junitReporter: {
-      outputDir: 'test-results',
-      outputFile: 'junit-results.xml',
-      useBrowserName: false
-    }
+      frameworks: ['jasmine'], // ou 'mocha', selon votre préférence
+      files: [
+          // vos fichiers de test
+      ],
+      browsers: ['ChromeHeadless'], // Utiliser Chrome en mode headless
+      // Autres configurations
   });
 };
